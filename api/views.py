97ce4514b4aed8ad1f3107326f29847bd5664955
aakash2410 <aakash.sangani2410@gmail.com>
2021-05-03 15:44:55 +0530

    a

--- api/views.py
+++ api/views.py
@@ -4,6 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from .serializers import *
 from .models import *
+from rest_framework import generics
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView
 from django.contrib.auth.models import User
